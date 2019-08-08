cbc400fa40621ca7384deb1dffa6c24c5b627385
Albert van Dalen <albertfora@avdweb.nl>
2019-08-08 11:39:08 +0200

    Add files via upload

---
+++ avdweb_DAC16.h
@@ -0,0 +1,15 @@
+#ifndef DAC16_H
+#define DAC16_H
+
+class DAC16
+{ 
+public:
+  DAC16(byte SYNCpin, byte SCLKpin, byte DINpin);
+  void write(unsigned dacValue); 
+  
+private:
+  byte SYNCpin, SCLKpin, DINpin;
+};
+
+#endif
+
