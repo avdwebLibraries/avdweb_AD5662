062fdff370287479d3e1afa00a6db207668b26fd
Albert van Dalen <albertfora@avdweb.nl>
2019-08-08 11:42:13 +0200

    Add files via upload

---
+++ Examples/Test_avdweb_DAC16.ino
@@ -0,0 +1,26 @@
+#if ARDUINO >= 100 
+  #include "Arduino.h"
+#else
+  #include "WProgram.h"
+#endif
+
+#include "avdweb_DAC16.h"
+
+const byte SYNCpin = 2;
+const byte SCLKpin = 3; 
+const byte DINpin = 4;
+
+DAC16 dac16(SYNCpin, SCLKpin, DINpin);
+
+void setup(void) 
+{ Serial.begin(9600);
+  dac16.write(32767); // test first write 2.5V
+  delay(3000);
+  dac16.write(0);
+}
+
+void loop(void) 
+{ for(unsigned i=0; i<65536; i+=1024) dac16.write(i); // create a sawtooth 
+}
+
+ 
